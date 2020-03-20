ce5234148559c5edd67c2e0ecfcc6205243174ba
Lam Jun Rong <bobo999bobo@outlook.com>
2020-03-20 14:49:21 +0800

    Add telemetry on token usage

--- src/main/kotlin/com/junron/pyrostore/apis/Routes.kt
+++ src/main/kotlin/com/junron/pyrostore/apis/Routes.kt
@@ -1,7 +1,9 @@
 package com.junron.pyrostore.apis
 
 import io.ktor.application.call
+import io.ktor.features.origin
 import io.ktor.http.ContentType
+import io.ktor.request.receiveText
 import io.ktor.response.respondText
 import io.ktor.routing.Route
 import io.ktor.routing.get
@@ -15,6 +17,17 @@ fun Route.apis() {
     }
 
     get("covid-19") {
+        with(call) {
+            val token =
+                request.headers["x-pyrobase-api-token"] ?: request.queryParameters["pyrobase-api-token"]
+                ?: receiveText().substringAfter("\"pyrobase-api-token\":\"").substringBefore("\"")
+            val ip = request.origin.remoteHost
+            if (token.isBlank()) {
+                println("IP $ip Token not used!")
+            } else {
+                println("IP $ip Token: $token")
+            }
+        }
         Scrape.initDocument()
 
         val fullResponse = FullResponse(
